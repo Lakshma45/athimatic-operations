82f8dfad21e65c0a3645b660d5dec0fa066ded05
Lakshma45 <111054201+Lakshma45@users.noreply.github.com>
2022-11-24 20:24:50 +0530

    Add files via upload

---
+++ arithmatic operations.py
@@ -0,0 +1,16 @@
+print('enter the values')
+a=int(input())
+b=int(input())
+d=a*b
+print('multiplication',d)
+e=a+b
+print('add',e)
+r=a-b
+print('subs',r)
+c=a%b
+print('division',c)
+x=a**b
+print('power',x)
+z=a//b
+print('floor division',z)
+
